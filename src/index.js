1a9bef5afb1717eddd65cec0401ab448582d24a4
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-03-14 22:53:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0)
+    {
+        n = Math.abs(n);
+    }
+  let string = String(n);
+  let result = string.split("").reverse().join("");
+
+  return +result;
 }
